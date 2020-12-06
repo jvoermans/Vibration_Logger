ad18542a69f2fd25b1f3bb47e16153b6ccab9053
jerabaul29 <jean.rblt@gmail.com>
2020-12-06 20:10:17 +0100

    Add some testing framework

---
+++ material_Jean/Due_SD_high_frequency_logger/test/tests_local/tests_FastLogger.cpp
@@ -0,0 +1,15 @@
+#include <FastLogger.h>
+#include <unity.h>
+
+void test_failing(void) {
+    TEST_ASSERT_EQUAL(32, 32);
+}
+
+
+int main(int argc, char **argv) {
+    UNITY_BEGIN();
+    RUN_TEST(test_failing);
+    UNITY_END();
+
+    return 0;
+}
